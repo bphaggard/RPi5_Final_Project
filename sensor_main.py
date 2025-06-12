9e8dcf5a3823b796c7d15a21e81f5efc0b7cb904
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-06-12 17:22:06 +0200

    Added function for sending email alert with photo. Gitignore file

--- sensor_main.py
+++ sensor_main.py
@@ -1,15 +1,22 @@
 from gpiozero import LED, MotionSensor
 import time
 import os
+import yagmail
 from picamzero import Camera
 from signal import pause
 from datetime import datetime
+from dotenv import load_dotenv
 
 # Initialize LED, PIR motion sensor and Camera using GPIO Zero library
 led = LED(22)  # LED connected to GPIO pin 22
 pir = MotionSensor(17)  # PIR sensor connected to GPIO pin 17
 camera = Camera()
+
 last_time_photo_taken = 0
+
+load_dotenv()
+email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
 
 def motion_detected():
     print("Start motion timer")
@@ -25,10 +32,12 @@ def motion_finished():
         if time.time() - last_time_photo_taken > 30.0:
             last_time_photo_taken = time.time()
             take_photo()
+            send_photo_by_email()
     else:
         print(f"Motion duration: {motion_duration}")
 
 def take_photo():
+    global filepath
     photo_folder = "/home/haggard/Desktop/Final_Project/photos"
     datetime_value = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     filename = f"img_{datetime_value}.jpg"
@@ -45,10 +54,15 @@ def take_photo():
         os.remove(log_file)
         print("Previous log file was removed")
     with open(log_file, mode="a") as file:
-        file.write(f"\n{filepath}")
+        file.write(f"{filepath}\n")
 
 def send_photo_by_email():
-    pass
+    yag_mail = yagmail.SMTP(email, password)
+    yag_mail.send(to=email, 
+              subject="Motion Alert!", 
+              contents="Motion was detected. Check image captured by camera in attachment",
+              attachments=filepath)
+    print("Email alert has been sent")
 
 pir.when_motion = motion_detected
 pir.when_no_motion = motion_finished
